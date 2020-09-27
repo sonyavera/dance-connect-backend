be0f16056114a9c8263b8be251daf7074640e30f
sonyavera <sonyavgould@gmail.com>
2020-09-27 15:52:37 -0400

    halfway through pic upload feature

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,6 +7,8 @@ class Api::V1::UsersController < ApplicationController
  
   def create
     user = User.create(user_params)
+    user.image.attach(user_params[:avatar])
+    byebug
     if user.valid?
       token = encode_token({ user_id: user.id })
       render json: { user: UserSerializer.new(user), jwt: token }, status: :created
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,9 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :username, :account_type, :first_name, :last_name, :id, :avatar
+
+
+
+  def avatar 
+    rails_blob_path(object.avatar, disposition: 'attachment', only_path: true) if object.avatar.attached?
+  end 
 end
