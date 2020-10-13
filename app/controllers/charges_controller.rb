e3ab7c0192bfc42f0aa2df74f59514a54e07baee
sonyavera <sonyavgould@gmail.com>
2020-10-13 11:06:29 -0400

    pre webhook implementation

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -8,47 +8,26 @@ class ChargesController < ApplicationController
     # end
     
     def create
-
+        dance_class = DanceClass.find_by_id(params[:class_id])
         session = Stripe::Checkout::Session.create({
             payment_method_types: ['card'],
             line_items: [{
                 price_data: {
-                    unit_amount: 2000,
+                    unit_amount: dance_class.price*100,
                     currency: 'usd',
                     product_data: {
-                        name: 'Stubborn Attachments',
-                        images: ['https://i.imgur.com/EHyR2nP.png'],
+                        name: dance_class.style + " with " + dance_class.instructor_name,
+                        images: [dance_class.instructor_avatar],
                     },
                 },
                 quantity: 1,
                 }],
                 mode: 'payment',
-                success_url: "http://localhost:3001/checkout" + '?success=true',
-                cancel_url: "http://localhost:3001/checkout" + '?canceled=true',
-                })
-                render json: {id: session.id}
+                # success_url: "http://localhost:3001/me/purchases",
+                success_url: "http://localhost:3001/me/purchases" + '?success=true',
+                cancel_url: "http://localhost:3001/me/purchases" + '?canceled=true',
+            })
+        render json: {id: session.id}
     end
     
 end
-        # Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-
-
-        # byebug
-        # Stripe.api_key = ENV['STRIPE_SECRET_KEY']
-        # begin
-            
-        #     customer = Stripe::Customer.create(
-        #         email => "svg145@gmail.com",
-        #         :source => params[:charge][:token]
-        #     )
-
-        #     charge = Stripe::Charge.create({
-        #         :customer => customer.id,
-        #         :amount => "2000",
-        #         :description => "dance class",
-        #         :currency => "usd",
-        #     })
-
-        #     rescue Stripe::CardError => e
-        #         render json: { message: 'card error' }, status: :not_acceptable
-        #     end
