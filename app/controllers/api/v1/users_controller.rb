246dc207cbf95eac072fc5f7ae37c93a6c14ab23
sonyavera <sonyavgould@gmail.com>
2020-10-14 17:53:22 -0400

    working on patch user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -7,8 +7,6 @@ class Api::V1::UsersController < ApplicationController
   end
  
   def create
-    # user = User.create(user_params)
-    # user.avatar.attach(user_params[:avatar])
     user = User.create(first_name: params[:first_name], 
                         last_name: params[:last_name],
                         username: params[:username],
@@ -25,10 +23,27 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def update
-    byebug
-    # updated_user = current_user.update_attributes(params[:user_params])
-    updated_user = current_user.update_attributes(params[:user])
-    render json: {user: updated_user}
+    if params[:first_name] != "null"
+      current_user.update(first_name: params[:first_name])
+    end
+    if params[:last_name] != "null"
+      current_user.update(last_name: params[:last_name])
+    end
+    if params[:username] != "null"
+      current_user.update(username: params[:username])
+    end
+    if params[:password] != "null"
+      current_user.update(password: params[:password])
+    end
+    if params[:account_type] != "null"
+      current_user.update(account_type: params[:account_type])
+    end
+    if params[:avatar] != "null"
+      current_user.avatar.purge_later
+      # current_user.avatar.attach(params[:avatar])
+    end
+    
+    render json: {user: current_user}
   end
  
   private
